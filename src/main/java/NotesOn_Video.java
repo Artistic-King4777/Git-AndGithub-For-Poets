defc067416d4eacdee14be57805164d680212951
NightShadow3000 <RoyalFotoK@gmail.com>
2018-09-27 20:24:12 -0400

    git hub help

---
+++ src/main/java/NotesOn_Video.java
@@ -0,0 +1,68 @@
+public class NotesOn_Video {
+
+      /*
+      Watching this playlist for instructions
+       - https://www.youtube.com/watch?v=BCQHnlnPusY&list=PLRqwX-V7Uu6ZF9C0YMKuns9sLDzK6zoiV&index=0
+        - his name on github is RainbowCoder
+        - and Shiffman
+
+      Best for Version Control and Collaboration
+
+      Git   &   Center   &  GitHub
+       - Git is a Version Control Software
+          - this is the actual application
+        - GitHub is a webservice tht uses github and vice versa
+         - its a website that has projects and you can do version control on them
+       - if you want u could only use git and never git hub but u could also only use github and never use git anywhere else
+
+
+       Repository or "Repo"
+         -  a repo for files
+         - holds your commits
+
+       Made a Txt File in GitHub (didnt knw could do tht)
+        - rainbow poem
+
+       Commit is a save
+        - i am going to change the file and make a commit
+        - You can edit files directly in the gitHUB!
+        - u can look at the history of your commits if u click history
+        - there's something called a Commit hash
+        - its a unique identifier for THAT commit
+
+
+        BRANCH:
+         - u can branch off where u can experiment
+         - first initial is named Master branch
+         - u can add branches that branch themselves till its like a tree
+         - u select the branch: master  button and create a new branch
+         and commit to the unicorn branch
+          - graphs button is now the Insights button then u click network
+
+
+        PULL REQUEST and MERGE
+         - Will u please take my branch and add it to master
+         take some changes from a particular branch and bring them into another branch
+         - git checks for if it can merge
+         - u only get a problem if uve edited a line already there
+         - u can add a new branch and then change the master at same time// and then
+         merge said commits into one even tho the masters ahead of the branch now
+         - EXPERIMENT LATER
+
+        FORK AND PULL REQUEST
+         - U can fork someone elses code (no initial or known things)
+         - if you do that then u dont have to worry about  affecting someone elses code?
+         - PULL REQUEST wanting to contribute this code to the other persons pull request which will then
+         accepted or rejected
+         - sending out code is PUSHING
+         - taking in code is PULLING
+         - i as JOJO want to push my code to ZEE and see if he will PULL it in
+
+        -- SIMPLE WAY: open someones project and click edit file (will automatically fork since u don't have permission)
+          - another is fork button
+
+        STARTED ZEE COMMIT FOR DRAGONS
+
+     */
+
+}
